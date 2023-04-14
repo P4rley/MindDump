0f7988faf64fedd17d55d271ad29a700842b5230
Adwitya Hersa <adwitya@ndt-partners.com>
2023-04-14 09:56:56 +0700

    Init Project

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.wrapper}>
+        <Text style={styles.logo}>
+          <Text style={{ fontWeight: 500 }}>Mind</Text>Dump
+        </Text>
+        <Icon name="search" size={30} color="#000" />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  wrapper: {
+    display: "flex",
+    paddingHorizontal: 10,
+    alignItems: "center",
+    justifyContent: "space-between",
+    flexDirection: "row",
+  },
+  logo: {
+    fontWeight: 800,
+    fontSize: 24,
   },
 });
